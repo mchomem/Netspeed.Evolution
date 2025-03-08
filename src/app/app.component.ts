bdbde903469cf9891442b0ce40e8bae60dd65c75
Gabriel Peixoto <gabr.art.design@gmail.com>
2025-03-08 16:20:36 -0300

    feat(navbar): created navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/ui/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+  <app-navbar [title]="title" />
+  <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
-  title = 'NetspeedEvolution';
+  public title = 'Netspeed Evolution';
 }
