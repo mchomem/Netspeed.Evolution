aca503b333aa1f2bcddba027e922fe01ede8b29d
Misael C. Homem <misael.homem@gmail.com>
2025-03-10 01:45:46 -0300

    feat(home): enhance home page with task table and mock data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  tasks: Task[] = [];
 
+  constructor() {}
+  
+  ngOnInit() {
+    this.getTasksMock();
+  }
+
+  getTasksMock(){
+    this.tasks = [
+      { id: 1, name: 'Preencher formulário SWOT', state: 'Pendente' },
+      { id: 2, name: 'Preencher Plano de Ação 5W2H', state: 'Pendente' },
+    ];
+  }
 }
